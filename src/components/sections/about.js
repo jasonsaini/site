419a6a0508a30e3cc001de315df8e26fef1b012c
Jason Saini <jasonsaini.exe@gmail.com>
2023-10-15 12:49:38 -0400

    removed unused projects

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -100,7 +100,7 @@ const StyledPic = styled.div`
     &:before {
       top: 0;
       left: 0;
-      background-color: var(--navy);
+      background-color: var(--lightest-slate);
       mix-blend-mode: screen;
     }
 
