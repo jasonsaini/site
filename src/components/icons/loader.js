cb361d241966b1d56e22b7eeb66d6cc8351e766c
Jason Saini <jasonsaini.exe@gmail.com>
2023-11-03 14:07:16 -0400

    Testing load

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,5 +1,88 @@
 import React from 'react';
-import { ReactComponent as LocalIcon } from './icon.svg';
-const IconLoader = () => <LocalIcon />;
+
+const IconLoader = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <rect x="19" y="19" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0s"
+        calcMode="discrete"></animate>
+    </rect>
+    <rect x="40" y="19" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0.125s"
+        calcMode="discrete"></animate>
+    </rect>
+    <rect x="61" y="19" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0.25s"
+        calcMode="discrete"></animate>
+    </rect>
+    <rect x="19" y="40" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0.875s"
+        calcMode="discrete"></animate>
+    </rect>
+    <rect x="61" y="40" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0.375s"
+        calcMode="discrete"></animate>
+    </rect>
+    <rect x="19" y="61" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0.75s"
+        calcMode="discrete"></animate>
+    </rect>
+    <rect x="40" y="61" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0.625s"
+        calcMode="discrete"></animate>
+    </rect>
+    <rect x="61" y="61" width="20" height="20" fill="#495670">
+      <animate
+        attributeName="fill"
+        values="#df1f2d;#495670;#495670"
+        keyTimes="0;0.125;1"
+        dur="1s"
+        repeatCount="indefinite"
+        begin="0.5s"
+        calcMode="discrete"></animate>
+    </rect>
+  </svg>
+);
 
 export default IconLoader;
