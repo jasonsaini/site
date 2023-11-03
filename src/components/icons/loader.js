62f460e21b3703273b1057f7749e33f4776af861
Jason Saini <jasonsaini.exe@gmail.com>
2023-11-03 13:49:25 -0400

    Testing load

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,25 +1,8 @@
 import React from 'react';
-
+import { ReactComponent as LocalIcon } from './icon.svg';
 const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <title>Loader Logo</title>
-    <g>
-      <g id="B" transform="translate(11.000000, 5.000000)">
-        <path d="M10 0 L10 60 Q10 70 20 70 L40 70" fill="currentColor" />
-      </g>
-      <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
-      />
-    </g>
+    <LocalIcon />
   </svg>
 );
 
