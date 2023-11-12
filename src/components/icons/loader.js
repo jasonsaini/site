3ea9c1847517f7cfdcab857b4747fe6ef74174f1
Jason Saini <jasonsaini.exe@gmail.com>
2023-11-12 16:21:58 -0500

    removed thing

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -15,18 +15,6 @@ const IconLoader = () => (
             values="0 50 51;360 50 51"></animateTransform>
         </path>
       </g>
-      <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
-      />
     </g>
   </svg>
 );
