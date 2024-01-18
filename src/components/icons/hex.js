9d4a22f1f36ba1523fa7480ec0234ad582bf758a
Jason Saini <jasonsaini.exe@gmail.com>
2024-01-17 23:16:17 -0500

    enabled svg background

--- src/components/icons/hex.js
+++ src/components/icons/hex.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const IconHex = () => (
 
-<svg fill="#000000"  xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" >
+<svg  enableBackground="new 0 0 2335 2441.8"
+    viewBox="0 0 2335 2441.8" fill="#000000"  xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" >
   <g stroke="#000" strokeMiterlimit="10">
   <path d='M5.243 7.071l-4.95-4.95A1 1 0 1 1 1.707.707l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l4.95-4.95zM6.929 12h8a1 1 0 0 1 0 2h-8a1 1 0 0 1 0-2z' />
   </g>
